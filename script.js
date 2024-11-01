8a61e9faa966424efdb7898a296ecad26471e2ba
ipior <piotr.szaran@mail.utoronto.ca>
2024-11-01 15:11:16 -0400

    Modal bug fixed, made the elemnts divs so information is easier to display on the page

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const container = document.getElementById("calendar");
 const form = document.getElementById("activity-form");
 const calendarHeader = document.getElementById("calendar-header");
 const activityHeader = document.getElementById("activity-header");
-let buttonsEnabled = true;
-let formEnabled = true;
+let elementsEnabled = true;
+let inputSelected = "";
 
+const activitySaveButton = document.getElementById("activity-submit");
+const activityCloseButton = document.getElementById("activity-close");
 const activityName = document.getElementById("activity-name");
 const activityDuration = document.getElementById("activity-duration");
 const activityLocation = document.getElementById("activity-location");
 const activityDescription = document.getElementById("activity-description");
-const activitySaveButton = document.getElementById("activity-submit");
-const activityCloseButton = document.getElementById("activity-close");
 
 // Upon loading the window, create the calendar
 window.onload = () => {
@@ -18,7 +18,7 @@ window.onload = () => {
     // Function to create a row
     const createLabel = (title, rowCol) => {
         const label = document.createElement("div");
-        rowCol === "row" ? label.className = "rowHeader" : label.classList.add("colHeader", "sticky"); 
+        rowCol === "row" ? label.classList.add("rowHeader") : label.classList.add("colHeader", "sticky"); 
         label.textContent = title;
         container.appendChild(label);
     }
@@ -35,11 +35,11 @@ window.onload = () => {
         
         createLabel(time, "row")
         daysOfTheWeek.forEach(day => {
-            const input = document.createElement("button");
-            input.type = "text";
+            const input = document.createElement("span");
+            // input.type = "text";
             input.id = `${day}-${time}`;
             time.slice(3,5) === "30" ? input.className = "hour-mark" : input.className = "half-hour-mark";
-            input.classList.add(day)
+            input.classList.add(day, "timeslot", "hover")
             input.ariaLabel = `${day}-${time}`;
             input.onclick = update;
             container.appendChild(input);
@@ -48,98 +48,105 @@ window.onload = () => {
 
 }
 
-// const addEvent = (event) => {
-//     console.log(event.target)
-// }
 
 // Onchange to input, this function is called.
 const update = event => {
-    const element = event.target;
-    triggerForm();
-
-    if (formEnabled){
-
-        var promise = new Promise(resolve => {
-            
-        })
-    }
-
-    activitySaveButton.addEventListener("click", () => {
-        element.innerText = activityName.value;
-        triggerForm();
-    });
-
-
-    activityCloseButton.addEventListener("click", () => {
-
-        triggerForm();
-    });
-
-
-    return
+    inputSelected = event.target;
+    openModal();
 };
 
+const openModal = () => {
+    // Bring up the form 
+    form.style.zIndex = "1";
+    form.classList.remove("hidden");
+    activityHeader.classList.remove("hidden");
+    // "Pause" the calendar
+    container.classList.add("stop-scrolling");
+    container.style.opacity = "10%";
+    calendarHeader.style.opacity = "10%";
+    disableElements()
+};
 
+const closeModal = () => {
+    //Clean the values
+    activityName.value = "";
+    activityDuration.value = "30-minutes"
+    activityLocation.value = "";
+    activityDescription.value = "";
 
+    // Else, do the reverse action
+    form.style.zIndex = "0";
+    form.classList.add("hidden");
+    activityHeader.classList.add("hidden");
 
-
-
-const triggerForm = () => {
-    // If the form is hidden, show it
-    if (form.classList.contains("hidden")){
-        activityName.value = "";
-        activityLocation.value = "";
-        activityDescription.value = "";
-
-        // Bring up the form 
-        form.style.zIndex = "1";
-        form.classList.remove("hidden");
-        activityHeader.classList.remove("hidden");
-        // "Pause" the calendar
-        container.classList.add("stop-scrolling");
-        container.style.opacity = "10%";
-        calendarHeader.style.opacity = "10%";
-        formEnabled = true;
-        disableButtons()
-        // return
-    }
-    else {
-        // Else, do the reverse action
-        form.style.zIndex = "0";
-        form.classList.add("hidden");
-        activityHeader.classList.add("hidden");
-
-        container.classList.remove("stop-scrolling");
-        container.style.opacity = "100%";
-        calendarHeader.style.opacity = "100%";
-
-        formEnabled = false;
-        disableButtons()
-    }
-
-    // activitySaveButton.addEventListener("click", () => {
-    //     triggerForm();
-    // });
-
-
-    // activityCloseButton.addEventListener("click", () => {
-
-    //     triggerForm();
-    // });
-    return
-
+    container.classList.remove("stop-scrolling");
+    container.style.opacity = "100%";
+    calendarHeader.style.opacity = "100%";
+    disableElements()
 }
 
-const disableButtons = () => {
-    const buttons = document.querySelectorAll("button");
+activitySaveButton.addEventListener("click", () => {
+    
+    if (activityName.value === ""){
+        // alert("Please add a name")
+        return
+    }
 
-    if (buttonsEnabled) {
-        buttons.forEach(button => button.disabled=true)
-        buttonsEnabled = false;
+    // If the hour period is selected we need to identify the next input to style
+    if (activityDuration.value === "60-minutes") {
+        const secondInput = document.getElementById(getNextInput(inputSelected.id));
+        console.log(secondInput)
     }
     else {
-        buttons.forEach(button => button.disabled=false);
-        buttonsEnabled =true;
+        // inputSelected.id.innerHTML = `<p><${activityName}</p>`;
+        // inputSelected
+    }
+
+
+    closeModal();
+});
+
+activityCloseButton.addEventListener("click", closeModal);
+
+const getNextInput = (str) => {
+    let day = str.match(/^.*?-/i);
+    let time = str.match(/\d{2}:\d{2}/)[0];
+    let hour = Number(time.slice(0,2));
+    let minute = time.slice(3,5);
+
+    // If it a 30 minute mark, hour will have to increment
+    if (minute === "30"){
+        hour += 1;
+        minute = "00";
+
+        // Keeping this in american time
+        if (hour > 12){
+            hour = hour % 12;
+        }
+        if (hour > 9){
+            return `${day}${hour}:${minute} PM`;
+        }
+        return `${day}0${hour}:${minute} PM`;
+    }
+    else {
+        minute = "30";
+        if (hour > 9){
+            return `${day}${hour}:${minute} ${str.slice(-2)}`;
+        }
+        return `${day}0${hour}:${minute} ${str.slice(-2)}`;
+    }
+}
+
+const disableElements = () => {
+    const elements = [...document.getElementsByClassName("timeslot")];
+
+    if (elementsEnabled) {
+        elements.forEach(el => el.classList.remove("hover"))
+        elementsEnabled = false;
+    }
+    else {
+        elements.forEach(el => el.classList.add("hover"));
+        elementsEnabled =true;
     }
     return
 }
